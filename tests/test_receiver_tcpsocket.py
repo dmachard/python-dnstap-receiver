aac492c09002870adff4858f359cc850dbe7cb4b
dmachard <d.machard@gmail.com>
2020-09-29 20:45:25 +0200

    new unitests

---
+++ tests/test_receiver_tcpsocket.py
@@ -0,0 +1,38 @@
+
+import time
+import unittest
+import subprocess
+import dns.resolver
+
+my_resolver = dns.resolver.Resolver(configure=False)
+my_resolver.nameservers = ['127.0.0.1']
+
+class TestRcvTCPsocket(unittest.TestCase):
+    def test_listening(self):
+        """test listening tcp socket"""
+        cmd = ["python3", "-c", 
+               "from dnstap_receiver.receiver import start_receiver; start_receiver()",
+               "-v"]
+
+        with subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT) as proc:
+            time.sleep(1)
+            proc.kill()
+            
+            o = proc.stdout.read()
+        self.assertRegex(o, b"listening on")
+        
+    def test_incoming_dnstap(self):
+        """test to receive dnstap message"""
+        cmd = ["python3", "-c", 
+               "from dnstap_receiver.receiver import start_receiver; start_receiver()"]
+
+        with subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT) as proc:
+            for i in range(5):
+                r = my_resolver.resolve('www.github.com', 'a')
+                time.sleep(1)
+
+            proc.kill()
+            
+            o = proc.stdout.read()
+        self.assertRegex(o, b"CLIENT_RESPONSE")
+        
