a641dae6ae49f8b61dd32cce5dda33a7ca8f03b5
dmachard <d.machard@gmail.com>
2020-09-30 20:16:22 +0200

    new test for unix socket

--- tests/test_receiver_unixsocket.py
+++ tests/test_receiver_unixsocket.py
@@ -7,31 +7,31 @@ import dns.resolver
 my_resolver = dns.resolver.Resolver(configure=False)
 my_resolver.nameservers = ['127.0.0.1']
 
-class TestTcpSocket(unittest.TestCase):
+class TestUnixSocket(unittest.TestCase):
     def test1_listening(self):
-        """test listening tcp socket"""
-        cmd = '/usr/bin/python3 -c "from dnstap_receiver.receiver import start_receiver; start_receiver()" -v'
+        """test listening unix socket"""
+        cmd = 'su - dnsdist -s /bin/bash -c \'/usr/bin/python3 -c "from dnstap_receiver.receiver import start_receiver; start_receiver()" -u /var/run/dnsdist/dnstap.sock -v\''
 
         with subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT) as proc:
             time.sleep(2)
-            proc.kill()
+            proc.terminate()
             
             o = proc.stdout.read()
             print(o)
-        self.assertRegex(o, b"listening on")
+        self.assertRegex(o, b"listening on /var/run/dnsdist/dnstap.sock")
         
     def test2_incoming_dnstap(self):
         """test to receive dnstap message"""
-        cmd = '/usr/bin/python3 -c "from dnstap_receiver.receiver import start_receiver; start_receiver()"'
+        cmd = 'su - dnsdist -s /bin/bash -c \'/usr/bin/python3 -c "from dnstap_receiver.receiver import start_receiver; start_receiver()" -u /var/run/dnsdist/dnstap.sock -v\''
 
         with subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT) as proc:
             for i in range(10):
                 r = my_resolver.resolve('www.github.com', 'a')
                 time.sleep(1)
 
-            proc.kill()
+            proc.terminate()
             
             o = proc.stdout.read()
             print(o)
-        self.assertRegex(o, b"dnsdist-tcp CLIENT_RESPONSE")
+        self.assertRegex(o, b"dnsdist-unix CLIENT_RESPONSE")
         
