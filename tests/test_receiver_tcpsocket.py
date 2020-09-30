eb7fe7eef44403173fd9fea50edc0d28b8bd3dc4
dmachard <d.machard@gmail.com>
2020-09-30 20:47:41 +0200

    add sudo

--- tests/test_receiver_tcpsocket.py
+++ tests/test_receiver_tcpsocket.py
@@ -10,7 +10,7 @@ my_resolver.nameservers = ['127.0.0.1']
 class TestTcpSocket(unittest.TestCase):
     def test1_listening(self):
         """test listening tcp socket"""
-        cmd = 'python3 -c "from dnstap_receiver.receiver import start_receiver; start_receiver()" -v'
+        cmd = 'sudo python3 -c "from dnstap_receiver.receiver import start_receiver; start_receiver()" -v'
 
         with subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT) as proc:
             time.sleep(2)
@@ -22,7 +22,7 @@ class TestTcpSocket(unittest.TestCase):
         
     def test2_incoming_dnstap(self):
         """test to receive dnstap message"""
-        cmd = 'python3 -c "from dnstap_receiver.receiver import start_receiver; start_receiver()"'
+        cmd = 'sudo python3 -c "from dnstap_receiver.receiver import start_receiver; start_receiver()"'
 
         with subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT) as proc:
             for i in range(10):
