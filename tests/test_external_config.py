07968868db9e6465280f46feda78c24dbfe99fe6
dmachard <d.machard@gmail.com>
2020-11-13 05:57:49 +0100

    fix testunit

--- tests/test_external_config.py
+++ tests/test_external_config.py
@@ -11,39 +11,40 @@ def execute_dnstap(cmd):
         proc.terminate()
         o = proc.stdout.read()
         print(o)
+    return o
         
 class TestExternalConfig(unittest.TestCase):
     def test1_verbose_enable(self):
         """test to load an external file"""
         cmd = 'python3 -c "from dnstap_receiver.receiver import start_receiver; start_receiver()" -c ./tests/dnstap_verbose.conf'
-        execute_dnstap(cmd)
+        o = execute_dnstap(cmd)
         
         self.assertRegex(o, b"External config file loaded")
         
     def test2_output_stdout_enable(self):
         """test to enable stdout output"""
         cmd = 'python3 -c "from dnstap_receiver.receiver import start_receiver; start_receiver()" -c ./tests/dnstap_verbose.conf'
-        execute_dnstap(cmd)
+        o = execute_dnstap(cmd)
         
         self.assertRegex(o, b"Output handler: stdout")
         
     def test2_output_metrics_enable(self):
         """test to enable metrics output"""
         cmd = 'python3 -c "from dnstap_receiver.receiver import start_receiver; start_receiver()" -c ./tests/dnstap_metrics.conf'
-        execute_dnstap(cmd)
+        o = execute_dnstap(cmd)
         
         self.assertRegex(o, b"Output handler: metrics")
         
     def test3_output_syslog_enable(self):
         """test to enable syslog output"""
         cmd = 'python3 -c "from dnstap_receiver.receiver import start_receiver; start_receiver()" -c ./tests/dnstap_syslog.conf'
-        execute_dnstap(cmd)
+        o = execute_dnstap(cmd)
         
         self.assertRegex(o, b"Output handler: syslog")
         
     def test4_output_tcp_enable(self):
         """test to enable tcp output"""
         cmd = 'python3 -c "from dnstap_receiver.receiver import start_receiver; start_receiver()" -c ./tests/dnstap_tcp.conf'
-        execute_dnstap(cmd)
+        o = execute_dnstap(cmd)
         
         self.assertRegex(o, b"Output handler: tcp")
